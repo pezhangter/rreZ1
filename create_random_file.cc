bb68f0f102b1ffb3ca0ca05c2ffce44f25532edf
TellyTenny <pezhangter@gmail.com>
2017-10-03 18:05:41 -0400

    1

---
+++ create_random_file.cc
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/timeb.h>
+
+int main(int argc, char *argv[]) {
+
+	char *filename = argv[1];
+	long total_bytes = strtol(argv[2], NULL, 10);
+	long block_size = strtol(argv[3], NULL, 10);
+	char buffer[block_size];
+
+	struct timeb t;
+	ftime(&t);
+	long start_in_ms = t.time * 1000 + t.millitm;
+
+	FILE *fp = fopen(filename, "w");
+
+	long bytes_done = 0;
+	while (bytes_done < total_bytes){
+		memset(&buffer, 0, block_size);
+		if (total_bytes - bytes_done < block_size){
+
+			random_array(buffer, total_bytes - bytes_done);
+
+		} else {
+
+			random_array(buffer, block_size);
+
+		}
+		fwrite(buf, 1, block_size, fp);
+		fflush(fp);
+	}
+	long end_in_ms = t.time * 1000 + t.millitm;	
+	fclose(fp);
+	printf("time: %ld ls with a block_size of %ld\n", end_in_ms - start_in_ms, block_size);
+
+  	return 0;
+}
---
+++ library.cc
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+ * populate a random array (which is already
+ * allocated with enough memory to hold n bytes.
+ */
+void random_array(char *array, long bytes){
+	time_t t;
+	srand((unsigned) time(&t));
+
+	int i;
+	for (i = 0; i < bytes; i++){
+		char c = 'A' + (rand() % 26);
+		array[i] = c;
+	}
+}
+
