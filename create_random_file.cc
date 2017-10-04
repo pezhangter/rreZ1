86c59eef9babf68bf0a1f39e0778ff101475d5fb
TellyTenny <pezhangter@gmail.com>
2017-10-04 17:34:10 -0400

    24

--- create_random_file.cc
+++ create_random_file.cc
@@ -35,12 +35,8 @@ int main(int argc, char *argv[]) {
 	}
 	ftime(&t);
 	long end_in_ms = t.time * 1000 + t.millitm;	
-	long duration = end_in_ms - start_in_ms;
-
 	fclose(fp);
-	printf("%f Bps\n %ld milliseconds with a block_size of %ld and a total_bytes of %ld\n", 
-		(double) total_bytes/duration * 1000,
-		duration, block_size, total_bytes);
+	printf("%ld milliseconds with a block_size of %ld\n", (end_in_ms - start_in_ms), block_size);
 
   	return 0;
 }
