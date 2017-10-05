4bbc61c2bc1fa0ec0d695a492fa12cc657ca0515
TellyTenny <pezhangter@gmail.com>
2017-10-04 21:23:00 -0400

    37

--- read_speed.py
+++ read_speed.py
@@ -4,7 +4,7 @@ import subprocess
 KB = 1024
 MB = 1024 * KB
 filename = "random_file"
-block_sizes = [1,2,3,4,5,6,7,8,9,10]#[100, 500, 1 * KB, 5 * KB, 10 * KB, 50 * KB, 100 * KB, 500 * KB, 1 * MB, 3 * MB]
+block_sizes = [100, 500, 1 * KB, 5 * KB, 10 * KB, 50 * KB, 100 * KB, 500 * KB, 1 * MB, 3 * MB]
 f = open('read_result.txt', 'w')
 for i in range(10):
 	process = subprocess.Popen(["./get_histogram", filename, "%d" % block_sizes[i]],stdout=PIPE)
